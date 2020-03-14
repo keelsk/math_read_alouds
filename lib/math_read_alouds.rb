8d64f762382c6aa03c1d9d8290bcfe9b0a97552e
Kirsten Keels <keelsk@gmail.com>
2020-03-14 02:39:11 +0000

    installed pry and nokogiri

--- lib/math_read_alouds.rb
+++ lib/math_read_alouds.rb
@@ -1,6 +1,9 @@
 require_relative "./math_read_alouds/version"
 require_relative "./math_read_alouds/cli"
 
+require 'pry'
+
+
 module MathReadAlouds
   class Error < StandardError; end
   # Your code goes here...
--- lib/math_read_alouds/cli.rb
+++ lib/math_read_alouds/cli.rb
@@ -1,5 +1,7 @@
+
 class MathReadAlouds::CLI
   def call
     puts "Welcome to Math Read Alouds! Choose a math topic to find literacy connections that can enhance your math instruction!"
+    binding.pry
   end
 end
