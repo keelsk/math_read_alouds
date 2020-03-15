a2dfb1964c14a7b62a8fa46be81a6d299be74104
Kirsten Keels <keelsk@gmail.com>
2020-03-15 01:16:24 +0000

    add get_user_topic method

--- lib/math_read_alouds/cli.rb
+++ lib/math_read_alouds/cli.rb
@@ -7,9 +7,9 @@ class MathReadAlouds::CLI
   def call
     puts "Welcome to Math Read Alouds! Choose a math topic to find literacy connections that can enhance your math instruction!"
     
-    get_user_topics
+    list_topics
     
-    # get_user_topics
+    # get_user_topic
     # get_books_for(topic)
     # display_books
     # get_selected_book(selected_book)
@@ -17,15 +17,17 @@ class MathReadAlouds::CLI
     #new = MathReadAlouds::Scraper.new.scrape_topics_page
   end
   
-  def get_user_topics
-    topics = ['Counting', 'Addition', 'Subtraction', 'Multiplication', 'Division', 'Fractions', 'Geometry', 'Measurement']
-    
+  def list_topics
+    math_topics = ['Counting', 'Addition', 'Subtraction', 'Multiplication', 'Division', 'Fractions', 'Geometry', 'Measurement']
     puts "Type the number of the topic you need literacy connections for:"
-    
-    
-    topics.each_with_index do |topic, index|
+    math_topics.each_with_index do |topic, index|
       puts "#{index + 1}. #{topic}"
     end
+  end
+  
+  def get_user_topic
+    puts "Type the number of the topic you would like to explore:"
+    @input = gets.chomp
   end
   
   
