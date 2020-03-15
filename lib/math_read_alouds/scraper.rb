3abf2b7a9f214af71d5324b14d0f6f6a44f78722
Kirsten Keels <keelsk@gmail.com>
2020-03-15 04:45:56 +0000

    add .scrape_read_alouds method

--- lib/math_read_alouds/scraper.rb
+++ lib/math_read_alouds/scraper.rb
@@ -12,16 +12,35 @@ class MathReadAlouds::Scraper
       
       topic_url << page_url
     end
-    
-        binding.pry
+    topic_url
   end
   
-  def self.scrape_read_alouds(read_aloud_url)
+  def self.scrape_read_alouds(topic_url)
+    book_html = open(topic_url)
+    book_doc = Nokogiri::HTML(book_html)
+    
+    book_info = {}
+    book_data = []
+    additional_book_url = []
+    additional_book_url << book_doc.css("div.responsive-row div.ImageBlock.ImageBlockCenter a").attribute('href').text
+    
+    book_doc.css("div.responsive-row").each do |book|
+      description = book.css('div.responsive_col-3 p').text.strip 
+      if description.length > 30
+        book_info[:title] = book.css('h4 b').text
+        book_info[:author] = book.css('h4').text.split('by ')[1]
+        book_info[:description] = description
+        book_data << book_info 
+      end
+    end
+    book_data
   end
   
   def self.scrape_book_url(book_url)
+    
   end
   
   def self.scrape_prices(amazon_url)
   end
+  
 end
