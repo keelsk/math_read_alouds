63dc88a8dffb141ee7bfbbbb93ff59db885449f8
Kirsten Keels <keelsk@gmail.com>
2020-03-15 05:17:45 +0000

    add Topic class

---
+++ lib/math_read_alouds/topic.rb
@@ -0,0 +1,15 @@
+class MathReadAlouds::Topic
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save 
+    @@all << self
+  end
+    
+end
