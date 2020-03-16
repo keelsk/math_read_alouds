c7089a12c0d51c4842c250db420599c504f75790
Kirsten Keels <keelsk@gmail.com>
2020-03-16 04:10:58 +0000

    modify Scraper Class

--- lib/math_read_alouds/scraper.rb
+++ lib/math_read_alouds/scraper.rb
@@ -11,10 +11,7 @@ class MathReadAlouds::Scraper
       page_url = content.css("a").attribute("href").value.split('.com')[1]
       @topic_url << page_url if !(@topic_url.include?(page_url))
     end
-    
   end
-  
-  
   
   def self.scrape_books(topic)
     url = @topic_url.select {|url| url.include?(topic.name.downcase)}.join
@@ -29,62 +26,6 @@ class MathReadAlouds::Scraper
         MathReadAlouds::Book.new(title, author, description, topic)
       end
     end
-  end
-  
-  
-  
-  # def self.scrape_topic_url
-  #   topic_url = []
-    
-  #   html = open('https://www.k-5mathteachingresources.com/math-read-alouds.html') #Change this and move url to CLI class. Save it as base_url + unique part of url
-  #   doc = Nokogiri::HTML(html)
-    
-  #   doc.css("div#ContentWrapper div.ImageBlock.ImageBlockCenter").each do |content|
-  #     page_url = content.css("a").attribute("href").value.split('.com')[1]
-  #     topic_url << page_url
-  #   end
-  #   topic_url
-  # end
-  
-  
-  
-  def self.scrape_read_alouds(topic_url)
-    book_html = open(topic_url)
-    book_doc = Nokogiri::HTML(book_html)
-    
-    book_info = {}
-    book_data = []
-    additional_book_url = []
-    additional_book_url << book_doc.css("div.responsive-row div.ImageBlock.ImageBlockCenter a").attribute('href').text
-    
-    book_doc.css("div.responsive-row").each do |book|
-      description = book.css('div.responsive_col-3 p').text.strip 
-      if description.length > 30
-        title = book.css('h4 b').text
-        author = book.css('h4').text.split('by ')[1]
-        
-        MathReadAlouds::Book.new(title, author, description)
-        
-        
-        #book_info = {:title => title, :author => author, :description => description}
-        #book_data << book_info
-      end
-    end
-    book_data
-    binding.pry
-  end
-  
-  
-  
-  
-  def self.scrape_book_url(book_url)
-    
-  end
-  
-  
-  
-  
-  def self.scrape_prices(amazon_url)
   end
   
 end
